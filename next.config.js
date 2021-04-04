fb0c45c7b9daaf21a8954c6885dcfcb9ce64e242
Victor Menezes <victormenezes8990@gmail.com>
2021-04-04 12:01:29 -0300

    fix: remove pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
+// const withPWA = require('next-pwa')
 const withReactSvg = require('next-react-svg')
 const path = require('path')
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  },
-  future: {
-    webpack5: true
-  }
-})
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     disable: !isProd
+//   },
+//   future: {
+//     webpack5: true
+//   }
+// })
 
 module.exports = withReactSvg({
   include: path.resolve(__dirname, 'src/assets/svg'),
